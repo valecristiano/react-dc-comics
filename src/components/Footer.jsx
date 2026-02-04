760358b2769b4259e3f8bb2b933ab50629cf83b6
valecristiano <valeriamdcristiano@gmail.com>
2026-02-04 19:18:48 +0300

    footer first structure

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,38 @@
 export default function Footer() {
-  return <footer>Sono un footer</footer>;
+  return (
+    <footer>
+      <div className="footer">
+        <div className="footer-bg d-flex">
+          <div className="container text-white">Dc Comics</div>
+          <img src="public/dc-logo-bg.png" alt="" />
+        </div>
+      </div>
+
+      <div id="follow-us">
+        <div className="container d-flex justify-content-between align-items-center py-3 ">
+          <button>SIGN-UP NOW</button>
+          <div className="icons">
+            <a href="#" className="px-3">
+              FOLLOW US
+            </a>
+            <a href="#" className="px-1">
+              <img src="public/footer-facebook.png" alt="Facebook" />
+            </a>
+            <a href="#" className="px-1">
+              <img src="public/footer-twitter.png" alt="Twitter" />
+            </a>
+            <a href="#" className="px-1">
+              <img src="public/footer-youtube.png" alt="Youtube" />
+            </a>
+            <a href="#" className="px-1">
+              <img src="public/footer-pinterest.png" alt="Pinterest" />
+            </a>
+            <a href="#" className="px-1">
+              <img src="public/footer-periscope.png" alt="Periscope" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
