280c5cbb373d12eaa8dfdfeaa37f69182ef999e5
valecristiano <valeriamdcristiano@gmail.com>
2026-02-05 18:25:26 +0300

    map for navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,72 +1,70 @@
+const navbarLinks = [
+  {
+    url: "#",
+    text: "CHARACTERS",
+  },
+  {
+    url: "#",
+    text: "COMICS",
+  },
+  {
+    url: "#",
+    text: "MOVIES",
+  },
+  {
+    url: "#",
+    text: "TV",
+  },
+  {
+    url: "#",
+    text: "GAMES",
+  },
+  {
+    url: "#",
+    text: "COLLECTIBLES",
+  },
+  {
+    url: "#",
+    text: "VIDEOS",
+  },
+  {
+    url: "#",
+    text: "FANS",
+  },
+  {
+    url: "#",
+    text: "NEWS",
+  },
+  {
+    url: "#",
+    text: "SHOP",
+  },
+];
+
 export default function Header() {
   return (
     <header>
-      <div className="container">
-        <nav className="navbar navbar-expand-lg">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="">
-              <img src="/dc-logo.png" alt="logoDc" height={50} />
-            </a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav ms-auto">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
-                    CHARACTERS
+      <nav className="navbar navbar-expand-lg bg-white mb-4">
+        <div className="container">
+          <a className="navbar-brand" href="">
+            <img src="/dc-logo.png" alt="logoDc" height={50} />
+          </a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              {navbarLinks.map((link, index) => (
+                <li key={index} className="nav-item">
+                  <a className="nav-link active" aria-current="page" href={link.url}>
+                    {link.text}
                   </a>
                 </li>
-                <li className="nav-item">
-                  <a className="nav-link active" href="#">
-                    COMICS
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link active" href="#">
-                    MOVIES
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link active" href="#">
-                    TV
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link active" href="#">
-                    GAMES
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link active" href="#">
-                    COLLECTIBLES
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link active" href="#">
-                    VIDEOS
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link active" href="#">
-                    FANS
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link active" href="#">
-                    NEWS
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link active" href="#">
-                    SHOP
-                  </a>
-                </li>
-              </ul>
-            </div>
+              ))}
+            </ul>
           </div>
-        </nav>
-      </div>
+        </div>
+      </nav>
     </header>
   );
 }
